a2e9946abd31a31f60edad18154b480337da465c
Ed McCormic <edmccormic@me.com>
2016-04-22 15:51:56 +0000

    Add account activations

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-
-  get 'users/new'
+ 
 
    root             'static_pages#home'
   get 'help'    => 'static_pages#help'
@@ -13,5 +11,5 @@ Rails.application.routes.draw do
   delete 'logout'  => 'sessions#destroy'
 
  resources :users
-  
+   resources :account_activations, only: [:edit]
 end
